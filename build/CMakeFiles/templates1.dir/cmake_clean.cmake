f5a0f8c8c14d8f5e6f7419afe0f3b5bfbaba8c6c
Anatoliy Shchepetkov <tolick.shepetkov61@gmail.com>
2025-01-24 20:05:12 +0300

    Boost asio first

---
+++ build/CMakeFiles/templates1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/templates1.dir/main.cpp.o"
+  "CMakeFiles/templates1.dir/main.cpp.o.d"
+  "templates1"
+  "templates1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/templates1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Headers/CMakeFiles/Headers.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Headers.
+# This may be replaced when dependencies are built.
