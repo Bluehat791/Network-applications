0fdf17d23b116848c4fd5ab76c77a25bcf306b3a
Anatoliy Shchepetkov <tolick.shepetkov61@gmail.com>
2025-01-31 20:09:46 +0300

    Network framework2

---
+++ NetClient/build/CMakeFiles/templates1.dir/SimpleClient.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/templates1.dir/SimpleClient.cpp.o: \
+ /home/anatoliy/projects/edu/Network-Applications/NetClient/SimpleClient.cpp \
+ /usr/include/stdc-predef.h
---
+++ NetClient/build/CMakeFiles/templates1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/templates1.dir/SimpleClient.cpp.o"
+  "CMakeFiles/templates1.dir/SimpleClient.cpp.o.d"
+  "templates1"
+  "templates1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/templates1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
